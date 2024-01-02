6a6004a6ead6b353c5abb890c6629c29f4681eb0
EstebanGomez <gomez.esteban@outlook.fr>
2024-01-02 23:18:51 +0100

    [ADD] vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
 import Home from "./pages/home/Home";
 import './common/i18n'
 import Projects from "./pages/projects/Projects";
 import Contact from "./pages/contact/Contact";
 import AboutMe from "./pages/aboutMe/AboutMe";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
     return (
-      <Router>
-        <Routes>
-          <Route path="" element={ <Home />} />
-          <Route path="/projects" element={ <Projects />} />
-          <Route path="/contact" element={ <Contact />} />
-          <Route path="/about-me" element={ <AboutMe />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Router>
-  );
+        <>
+            <Router>
+                <Routes>
+                    <Route path="" element={<Home/>}/>
+                    <Route path="/projects" element={<Projects/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                    <Route path="/about-me" element={<AboutMe/>}/>
+                    <Route path="*" element={<Navigate to="/"/>}/>
+                </Routes>
+            </Router>
+            <Analytics/>
+        </>
+    );
 }
 
 export default App;
